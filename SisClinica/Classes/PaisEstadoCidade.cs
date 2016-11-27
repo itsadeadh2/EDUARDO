8e3da5a53653712825ea396ba4a287766f0fe616
Thiago <Thiago@MALAKI>
2016-11-27 20:31:51 -0200

    Implementação da selecao de estado e cidade

---
+++ SisClinica/Classes/PaisEstadoCidade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SisClinica.DAO;
+
+namespace SisClinica.Classes
+{
+    public class PaisEstadoCidade
+    {
+        public int idPais { get; set; }
+        public int idEstado { get; set; }
+        public int idCidade { get; set; }
+        public string siglaPais { get; set; }
+        public string siglaEstado { get; set; }
+        public string nomeCidade { get; set; }
+
+        public IList<PaisEstadoCidade> BuscarTodosOsEstados()
+        {
+            return new PaisEstadoCidadeDAO().BuscarTodosOsEstados();
+        }
+        public PaisEstadoCidade BuscarCidade(int idCidade)
+        {
+            return new PaisEstadoCidadeDAO().BuscarCidade(idCidade);
+        }
+        public IList<PaisEstadoCidade> BuscarCidadesPorEstado(int idEstado)
+        {
+            return new PaisEstadoCidadeDAO().BuscarCidadesPorEstado(idEstado);
+        }
+        public PaisEstadoCidade BuscarEstado(int idEstado)
+        {
+            return new PaisEstadoCidadeDAO().BuscarEstado(idEstado);
+        }
+    }
+}
---
+++ SisClinica/DAO/PaisEstadoCidadeDAO.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SisClinica.Classes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SisClinica.DAO
+{
+    public class PaisEstadoCidadeDAO
+    {
+        public PaisEstadoCidade BuscarCidade(int idCidade)
+        {
+            PaisEstadoCidade objPaisEstadoCidade = new PaisEstadoCidade();
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "SELECT * FROM cidade WHERE cod_cidade = @id";
+            comando.Parameters.AddWithValue("@id", idCidade);
+
+            Conexao con = new Conexao();
+            SqlDataReader dr = con.ExecutarSelect(comando);
+
+            if (dr.HasRows)
+            {
+                dr.Read();
+                objPaisEstadoCidade.idCidade = (int)dr["cod_cidade"];
+                objPaisEstadoCidade.idEstado = (int)dr["cod_estado"];
+                objPaisEstadoCidade.nomeCidade = dr["nom_cidade"].ToString();
+            }
+            else
+            {
+                objPaisEstadoCidade = null;
+            }
+            return objPaisEstadoCidade;
+        }
+        public IList<PaisEstadoCidade> BuscarCidadesPorEstado(int idEstado)
+        {
+            IList<PaisEstadoCidade> listaPaisEstadoCidade = new List<PaisEstadoCidade>();
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "SELECT * FROM cidade WHERE cod_estado = @id";
+            comando.Parameters.AddWithValue("@id", idEstado);
+
+            Conexao con = new Conexao();
+            SqlDataReader dr = con.ExecutarSelect(comando);
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    PaisEstadoCidade objPaisEstadoCidade = new PaisEstadoCidade();
+                    objPaisEstadoCidade.idCidade = (int)dr["cod_cidade"];
+                    objPaisEstadoCidade.idEstado = (int)dr["cod_estado"];
+                    objPaisEstadoCidade.nomeCidade = dr["nom_cidade"].ToString();
+                    listaPaisEstadoCidade.Add(objPaisEstadoCidade);
+                }
+               
+            }
+            else
+            {
+                listaPaisEstadoCidade = null;
+            }
+            return listaPaisEstadoCidade;
+        }
+        public IList<PaisEstadoCidade> BuscarTodosOsEstados()
+        {
+            IList<PaisEstadoCidade> listaPaisEstadoCidade = new List<PaisEstadoCidade>();
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "select * from estado";
+
+            Conexao con = new Conexao();
+            SqlDataReader dr = con.ExecutarSelect(comando);
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    PaisEstadoCidade objPaisEstadoCidade = new PaisEstadoCidade();
+                    objPaisEstadoCidade.idCidade = (int)dr["cod_cidade"];
+                    objPaisEstadoCidade.idEstado = (int)dr["cod_estado"];
+                    objPaisEstadoCidade.nomeCidade = dr["nom_cidade"].ToString();
+                    objPaisEstadoCidade.siglaEstado = dr["sgl_estado"].ToString();
+                    listaPaisEstadoCidade.Add(objPaisEstadoCidade);
+                }
+            }
+            else
+            {
+                listaPaisEstadoCidade = null;
+            }
+            return listaPaisEstadoCidade;
+        }
+        public PaisEstadoCidade BuscarEstado(int idEstado)
+        {
+            PaisEstadoCidade objPaisEstadoCidade = new PaisEstadoCidade();
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "select * from estado where cod_estado = @id";
+            comando.Parameters.AddWithValue("@id", idEstado);
+
+            Conexao con = new Conexao();
+            SqlDataReader dr = con.ExecutarSelect(comando);
+            if (dr.HasRows)
+            {
+                dr.Read();
+                objPaisEstadoCidade.idCidade = (int)dr["cod_cidade"];
+                objPaisEstadoCidade.idEstado = (int)dr["cod_estado"];
+                objPaisEstadoCidade.nomeCidade = dr["nom_cidade"].ToString();
+                objPaisEstadoCidade.siglaEstado = dr["sgl_estado"].ToString();
+            }
+            else
+            {
+                objPaisEstadoCidade = null;
+            }
+            return objPaisEstadoCidade;
+        }
+    }
+}
